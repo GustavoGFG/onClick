82b466614c2338e719b19a43e721d22d9775fdc9
Gustavo Fernandes <goncalves_017@hotmail.com>
2024-01-24 18:58:04 -0300

    :recycle:Refactor: Data being fetched from mongo

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,5 +1,5 @@
-let url = 'https://gustavodev.pythonanywhere.com/products';
-
+// let url = 'https://gustavodev.pythonanywhere.com/products';
+let url = 'https://onclickstore-api.vercel.app/allproducts';
 async function getProducts(url) {
   let response = await fetch(url);
   let productListData = await response.json();
