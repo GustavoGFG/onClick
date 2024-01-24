82f2554d4be2a8be85027119262a0da158a99a3e
Gustavo Fernandes <goncalves_017@hotmail.com>
2024-01-24 17:02:11 -0300

    :bug:Fix logout

--- router.js
+++ router.js
@@ -93,7 +93,9 @@ const handleLocation = async () => {
     document
       .getElementById('yourorders-option')
       .addEventListener('click', () => showOrders(apiProductList));
-    document.getElementById('logout-btn').addEventListener('click', logout);
+    document
+      .getElementById('logout-btn')
+      .addEventListener('click', () => logout());
   }
 
   if (window.location.pathname === '/cart') {
